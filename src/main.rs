bc236312520d1dd936cf9c781061f8bf6f49628d
Felipe Jorge <felipe.jorge.pereira@gmail.com>
2021-05-09 01:35:43 -0300

    tangent editing

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,21 @@ use bevy::{
 };
 use bevy_egui::{egui, EguiContext, EguiPlugin};
 
+#[derive(Debug, PartialEq, Eq)]
+enum TangentEdit {
+    No,
+    In,
+    Out,
+}
+
 struct CurveEditor {
     dragging: bool,
     selected_keyframe: usize,
     display_offset: Vec2,
     display_range: Vec2,
     curve: CurveVariable<f32>,
-    popup: egui::Pos2,
+    tangent_popup_position: egui::Pos2,
+    tangent_drag: TangentEdit,
 }
 
 fn main() {
@@ -28,7 +36,8 @@ fn main() {
                 vec![3.0, 0.0, 1.0, 0.0, 0.5, 0.0, 0.25, 0.0],
             )
             .unwrap(),
-            popup: (0.0, 0.0).into(),
+            tangent_popup_position: (0.0, 0.0).into(),
+            tangent_drag: TangentEdit::No,
         })
         .add_plugins(DefaultPlugins)
         .add_plugin(EguiPlugin)
@@ -50,8 +59,9 @@ fn to_dir(a: f32) -> egui::Vec2 {
 }
 
 #[inline]
-fn to_tangent(y: f32, x: f32) -> f32 {
-    (-y).atan2(x)
+fn to_tangent(v: egui::Vec2) -> f32 {
+    let v = v.normalized();
+    v.y / v.x.abs().max(1e-12).copysign(v.x)
 }
 
 #[inline]
@@ -66,12 +76,12 @@ fn dot(
     color: egui::Color32,
 ) -> (bool, bool) {
     let offset = (select_radius, select_radius).into();
-    let keyframe_region = egui::Rect {
+    let rect = egui::Rect {
         min: position - offset,
         max: position + offset,
     };
 
-    if keyframe_region.contains(pointer_position) {
+    if rect.contains(pointer_position) {
         // Hovered
         painter.circle_filled(position, radius * 1.2, egui::Color32::WHITE);
         // Select on mouse down
@@ -140,104 +150,9 @@ fn ui_example(mut curve_editor: ResMut<CurveEditor>, egui_context: Res<EguiConte
                 }
             }
 
-            let popup_id = id.with("popup");
-
-            if let Some(pos) = response.hover_pos() {
-                if response.secondary_clicked() && !ui.memory().is_popup_open(popup_id) {
-                    curve_editor.popup = pos;
-                    ui.memory().open_popup(popup_id);
-                }
-            }
-
-            let temp = response.rect;
-            response.rect = egui::Rect::from_min_size(curve_editor.popup, (150.0, 1.0).into());
-            egui::popup::popup_below_widget(ui, popup_id, &response, |ui| {
-                let selected = curve_editor.selected_keyframe < curve_editor.curve.len();
-                ui.set_enabled(selected);
-
-                let index = curve_editor
-                    .selected_keyframe
-                    .min(CurveCursor::MAX as usize) as CurveCursor;
-
-                let (lerp_mode, tangent_mode) = if selected {
-                    (
-                        curve_editor.curve.get_interpolation(index),
-                        curve_editor.curve.get_tangent_control(index),
-                    )
-                } else {
-                    (Interpolation::Hermite, Default::default())
-                };
-
-                if ui
-                    .selectable_label(lerp_mode == Interpolation::Step, "Step")
-                    .clicked()
-                {
-                    curve_editor
-                        .curve
-                        .set_interpolation(index, Interpolation::Step);
-                }
-
-                if ui
-                    .selectable_label(lerp_mode == Interpolation::Linear, "Linear")
-                    .clicked()
-                {
-                    curve_editor
-                        .curve
-                        .set_interpolation(index, Interpolation::Linear);
-                }
-
-                ui.separator();
-
-                let hermite = lerp_mode == Interpolation::Hermite;
-                if ui
-                    .selectable_label(hermite && tangent_mode == TangentControl::Auto, "Auto")
-                    .clicked()
-                {
-                    curve_editor
-                        .curve
-                        .set_interpolation(index, Interpolation::Hermite);
-                    curve_editor
-                        .curve
-                        .set_tangent_control(index, TangentControl::Auto);
-                }
-                if ui
-                    .selectable_label(hermite && tangent_mode == TangentControl::Free, "Free")
-                    .clicked()
-                {
-                    curve_editor
-                        .curve
-                        .set_interpolation(index, Interpolation::Hermite);
-                    curve_editor
-                        .curve
-                        .set_tangent_control(index, TangentControl::Free);
-                }
-                if ui
-                    .selectable_label(hermite && tangent_mode == TangentControl::Flat, "Flat")
-                    .clicked()
-                {
-                    curve_editor
-                        .curve
-                        .set_interpolation(index, Interpolation::Hermite);
-                    curve_editor
-                        .curve
-                        .set_tangent_control(index, TangentControl::Flat);
-                }
-                if ui
-                    .selectable_label(hermite && tangent_mode == TangentControl::Broken, "Broken")
-                    .clicked()
-                {
-                    curve_editor
-                        .curve
-                        .set_interpolation(index, Interpolation::Hermite);
-                    curve_editor
-                        .curve
-                        .set_tangent_control(index, TangentControl::Broken);
-                }
-            });
-            response.rect = temp;
+            let curve = &mut curve_editor.curve;
 
             // Painter and style
-            let painter = ui.painter();
             let color = egui::Color32::RED;
             let stroke = egui::Stroke::new(1.0, color);
 
@@ -246,12 +161,100 @@ fn ui_example(mut curve_editor: ResMut<CurveEditor>, egui_context: Res<EguiConte
             let max = min + curve_editor.display_range;
             let duration = curve_editor.display_range.x.max(0.0);
 
+            // Context menu to change tangents
+            {
+                let popup_id = id.with("popup");
+
+                if let Some(pos) = response.hover_pos() {
+                    if response.secondary_clicked() && !ui.memory().is_popup_open(popup_id) {
+                        // // Relative to curve
+                        // pos.x = remap(rect.min.x, rect.max.x, pos.x, min.x, max.x);
+                        // pos.y = remap(rect.max.y, rect.min.y, pos.y, min.y, max.y);
+                        curve_editor.tangent_popup_position = pos;
+                        ui.memory().open_popup(popup_id);
+                    }
+                }
+
+                let temp = response.rect;
+                response.rect = egui::Rect::from_min_size(
+                    curve_editor.tangent_popup_position,
+                    (150.0, 1.0).into(),
+                );
+
+                let index = curve_editor
+                    .selected_keyframe
+                    .min(CurveCursor::MAX as usize) as CurveCursor;
+
+                egui::popup::popup_below_widget(ui, popup_id, &response, |ui| {
+                    let selected = (index as usize) < curve.len();
+                    ui.set_enabled(selected);
+                    let (lerp_mode, tangent_mode) = if selected {
+                        (
+                            curve.get_interpolation(index),
+                            curve.get_tangent_control(index),
+                        )
+                    } else {
+                        (Interpolation::Hermite, Default::default())
+                    };
+
+                    if ui
+                        .selectable_label(lerp_mode == Interpolation::Step, "Step")
+                        .clicked()
+                    {
+                        curve.set_interpolation(index, Interpolation::Step);
+                    }
+
+                    if ui
+                        .selectable_label(lerp_mode == Interpolation::Linear, "Linear")
+                        .clicked()
+                    {
+                        curve.set_interpolation(index, Interpolation::Linear);
+                    }
+
+                    ui.separator();
+
+                    let hermite = lerp_mode == Interpolation::Hermite;
+                    if ui
+                        .selectable_label(hermite && tangent_mode == TangentControl::Auto, "Auto")
+                        .clicked()
+                    {
+                        curve.set_interpolation(index, Interpolation::Hermite);
+                        curve.set_tangent_control(index, TangentControl::Auto);
+                    }
+                    if ui
+                        .selectable_label(hermite && tangent_mode == TangentControl::Free, "Free")
+                        .clicked()
+                    {
+                        curve.set_interpolation(index, Interpolation::Hermite);
+                        curve.set_tangent_control(index, TangentControl::Free);
+                    }
+                    if ui
+                        .selectable_label(hermite && tangent_mode == TangentControl::Flat, "Flat")
+                        .clicked()
+                    {
+                        curve.set_interpolation(index, Interpolation::Hermite);
+                        curve.set_tangent_control(index, TangentControl::Flat);
+                    }
+                    if ui
+                        .selectable_label(
+                            hermite && tangent_mode == TangentControl::Broken,
+                            "Broken",
+                        )
+                        .clicked()
+                    {
+                        curve.set_interpolation(index, Interpolation::Hermite);
+                        curve.set_tangent_control(index, TangentControl::Broken);
+                    }
+                });
+                response.rect = temp;
+            }
+
             // Curve rendering
             let mut t0 = min.x;
-            let (mut cursor, mut v0) = curve_editor.curve.sample_with_cursor(0, t0);
+            let (mut cursor, mut v0) = curve.sample_with_cursor(0, t0);
             for i in 1..256 {
                 let t1 = (duration * i as f32 / 255.0) + min.x;
-                let (next_cursor, v1) = curve_editor.curve.sample_with_cursor(cursor, t1);
+                let (next_cursor, v1) = curve.sample_with_cursor(cursor, t1);
 
                 let x0 = remap(min.x, max.x, t0, rect.min.x, rect.max.x);
                 let x1 = remap(min.x, max.x, t1, rect.min.x, rect.max.x);
@@ -259,7 +262,8 @@ fn ui_example(mut curve_editor: ResMut<CurveEditor>, egui_context: Res<EguiConte
                 let y0 = remap(min.y, max.y, v0, rect.max.y, rect.min.y);
                 let y1 = remap(min.y, max.y, v1, rect.max.y, rect.min.y);
 
-                painter.line_segment([egui::Pos2::new(x0, y0), egui::Pos2::new(x1, y1)], stroke);
+                ui.painter()
+                    .line_segment([egui::Pos2::new(x0, y0), egui::Pos2::new(x1, y1)], stroke);
 
                 v0 = v1;
                 t0 = t1;
@@ -283,9 +287,9 @@ fn ui_example(mut curve_editor: ResMut<CurveEditor>, egui_context: Res<EguiConte
             }
 
             // Render keyframes
-            for i in 0..curve_editor.curve.len() {
-                let t = curve_editor.curve.get_time(i as CurveCursor);
-                let v = *curve_editor.curve.get_value(i as CurveCursor);
+            for i in 0..curve.len() {
+                let t = curve.get_time(i as CurveCursor);
+                let v = *curve.get_value(i as CurveCursor);
 
                 let position = egui::Pos2::new(
                     remap(min.x, max.x, t, rect.min.x, rect.max.x),
@@ -302,12 +306,12 @@ fn ui_example(mut curve_editor: ResMut<CurveEditor>, egui_context: Res<EguiConte
                     let delta = position - pointer_position;
                     if (delta.y).abs() > 0.5 {
                         let v = remap(rect.max.y, rect.min.y, pointer_position.y, min.y, max.y);
-                        curve_editor.curve.set_value(i as CurveCursor, v);
+                        curve.set_value(i as CurveCursor, v);
                     }
 
                     if (delta.x).abs() > 0.5 {
                         let t = remap(rect.min.x, rect.max.x, pointer_position.x, min.x, max.x);
-                        let k = curve_editor.curve.set_time(i as CurveCursor, t);
+                        let k = curve.set_time(i as CurveCursor, t);
 
                         if let Some(k) = k {
                             // Keyframe ordering changed
@@ -319,52 +323,102 @@ fn ui_example(mut curve_editor: ResMut<CurveEditor>, egui_context: Res<EguiConte
 
                 if selected {
                     // Display tangents when selected
-                    let (a, b) = curve_editor.curve.get_in_out_tangent(i as CurveCursor);
+                    let index = i as CurveCursor;
+                    let lerp_mode = curve.get_interpolation(index);
 
-                    // In tangent
-                    let a = egui::Vec2::new(t, v) - to_dir(a);
-                    let a = egui::Pos2::new(
-                        remap(min.x, max.x, a.x, rect.min.x, rect.max.x),
-                        remap(min.y, max.y, a.y, rect.max.y, rect.min.y),
-                    );
-                    let a = position + (a - position).normalized() * 50.0;
-                    painter.line_segment([position, a], tangent_stroke);
-                    dot(
-                        painter,
-                        pointer_position,
-                        pointer_down,
-                        false,
-                        a,
-                        1.5,
-                        6.0,
-                        egui::Color32::GRAY,
-                    );
+                    if lerp_mode == Interpolation::Hermite {
+                        let (a, b) = curve.get_in_out_tangent(index);
 
-                    // Out tangent
-                    let b = egui::Vec2::new(t, v) + to_dir(b);
-                    let b = egui::Pos2::new(
-                        remap(min.x, max.x, b.x, rect.min.x, rect.max.x),
-                        remap(min.y, max.y, b.y, rect.max.y, rect.min.y),
-                    );
-                    let b = position + (b - position).normalized() * 50.0;
-                    painter.line_segment([position, b], tangent_stroke);
-                    dot(
-                        painter,
-                        pointer_position,
-                        pointer_down,
-                        false,
-                        b,
-                        1.5,
-                        6.0,
-                        egui::Color32::GRAY,
-                    );
+                        // In tangent
+                        let a = egui::Vec2::new(t, v) - to_dir(a);
+                        let a = egui::Pos2::new(
+                            remap(min.x, max.x, a.x, rect.min.x, rect.max.x),
+                            remap(min.y, max.y, a.y, rect.max.y, rect.min.y),
+                        );
+                        let a = position + (a - position).normalized() * 50.0;
+                        ui.painter().line_segment([position, a], tangent_stroke);
 
-                    // TODO: Edit tangents ...
+                        // Out tangent
+                        let b = egui::Vec2::new(t, v) + to_dir(b);
+                        let b = egui::Pos2::new(
+                            remap(min.x, max.x, b.x, rect.min.x, rect.max.x),
+                            remap(min.y, max.y, b.y, rect.max.y, rect.min.y),
+                        );
+                        let b = position + (b - position).normalized() * 50.0;
+                        ui.painter().line_segment([position, b], tangent_stroke);
+
+                        let tangent_mode = curve.get_tangent_control(index);
+                        if tangent_mode != TangentControl::Auto
+                            && tangent_mode != TangentControl::Flat
+                        {
+                            let selected = curve_editor.tangent_drag == TangentEdit::In;
+                            let (select, _) = dot(
+                                ui.painter(),
+                                pointer_position,
+                                pointer_down,
+                                selected,
+                                a,
+                                1.5,
+                                6.0,
+                                egui::Color32::GRAY,
+                            );
+                            if select && pointer_down {
+                                curve_editor.tangent_drag = TangentEdit::In;
+
+                                let p = egui::Pos2::new(
+                                    remap(rect.min.x, rect.max.x, pointer_position.x, min.x, max.x),
+                                    remap(rect.max.y, rect.min.y, pointer_position.y, min.y, max.y),
+                                );
+                                let tangent =
+                                    to_tangent(p - egui::Pos2::new(t, v)).clamp(-1e5, 1e5);
+
+                                if tangent_mode == TangentControl::Broken {
+                                    curve.set_in_tangent(index, tangent);
+                                } else {
+                                    curve.set_in_out_tangent(index, tangent);
+                                }
+                            } else if selected {
+                                curve_editor.tangent_drag = TangentEdit::No;
+                            }
+
+                            let selected = curve_editor.tangent_drag == TangentEdit::Out;
+                            let (select, _) = dot(
+                                ui.painter(),
+                                pointer_position,
+                                pointer_down,
+                                selected,
+                                b,
+                                1.5,
+                                6.0,
+                                egui::Color32::GRAY,
+                            );
+                            if select && pointer_down {
+                                curve_editor.tangent_drag = TangentEdit::Out;
+
+                                let p = egui::Pos2::new(
+                                    remap(rect.min.x, rect.max.x, pointer_position.x, min.x, max.x),
+                                    remap(rect.max.y, rect.min.y, pointer_position.y, min.y, max.y),
+                                );
+                                let tangent =
+                                    to_tangent(p - egui::Pos2::new(t, v)).clamp(-1e5, 1e5);
+
+                                if tangent_mode == TangentControl::Broken {
+                                    curve.set_out_tangent(index, tangent);
+                                } else {
+                                    curve.set_in_out_tangent(index, tangent);
+                                }
+                            } else if selected {
+                                curve_editor.tangent_drag = TangentEdit::No;
+                            }
+                        }
+
+                        // TODO: Edit tangents ...
+                    }
                 }
 
                 // Keyframe dot
                 let (select, press) = dot(
-                    painter,
+                    ui.painter(),
                     pointer_position,
                     pointer_down,
                     selected,
